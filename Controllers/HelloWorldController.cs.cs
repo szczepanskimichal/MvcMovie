12e64d43d99a1b98b513aef6fb5083839de41bae
User <user@example.com>
2026-02-25 19:23:38 +0100

    Initial commit: ASP.NET Core MVC Movie application - HelloWorldController with Index and Welcome actions - View templates for Index and Welcome - Passing data from controller to view using ViewData - Bootstrap styling with responsive layout

---
+++ Controllers/HelloWorldController.cs.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewData["MovieTitle"] = "Ghostbusters";
+        ViewData["NumTimes"] = 3;
+        return View();
+    }
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+}
